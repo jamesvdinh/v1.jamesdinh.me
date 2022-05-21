6697aa3bd10d1747c093b376bc1bb31db2bbf9de
James Dinh <jamesdinh77@gmail.com>
2022-05-21 15:22:03 -0700

    Simplified js

--- js/dynamic-background.js
+++ js/dynamic-background.js
@@ -4,6 +4,3 @@ var hours = date.getHours();
 if (hours > 20 || hours < 6) {
     document.write('<div class="entrance-background" style="background-image: url(../img/mountains.jpg);">')
 }
-else {
-    document.write('<div class="entrance-background" style="background-image: url(../img/mountains-2.jpg);">')
-}
