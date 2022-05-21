3f81034ce4faf138bfed0bc57cef1171adebca92
James Dinh <jamesdinh77@gmail.com>
2022-05-21 11:07:06 -0700

    Added js

---
+++ js/dynamic-background.js
@@ -0,0 +1,10 @@
+
+var date = new Date();
+var hours = date.getHours();
+if (hours > 18 || hours < 6) {
+    document.write('<div class="entrance-background" style="background-image: url(../img/mountains.jpg);">')
+
+}
+else {
+    document.write('<div class="entrance-background" style="background-image: url(../img/mountains-2.jpg);">')
+}
