23b33b5fce118eb94b396e0c7cbb53184a73da8b
James Dinh <jamesdinh77@gmail.com>
2022-05-21 13:03:24 -0700

    Changed js times

--- js/dynamic-background.js
+++ js/dynamic-background.js
@@ -1,7 +1,7 @@
 
 var date = new Date();
 var hours = date.getHours();
-if (hours > 18 || hours < 6) {
+if (hours > 20 || hours < 6) {
     document.write('<div class="entrance-background" style="background-image: url(../img/mountains.jpg);">')
 }
 else {
